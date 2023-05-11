073fbe79db200bfd0466f2eee822268cfd324741
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-11 14:45:15 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /01. Stacks and Queues - Lab/04.MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿//INPUT
+string input = Console.ReadLine();
+
+//ACTION
+Stack<int> indexes = new Stack<int>();
+List<string> expressions = new List<string>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '(')
+    {
+        indexes.Push(i);
+    }
+    else if (input[i] == ')')
+    {
+        int indexOfOpenBracket = indexes.Pop();
+        int indexOfClosedBracket = i;
+
+        string currentExpression = input.Substring(indexOfOpenBracket, indexOfClosedBracket - indexOfOpenBracket + 1);
+
+        expressions.Add(currentExpression);
+    }
+}
+
+//OUTPUT
+foreach (var expression in expressions)
+{
+    Console.WriteLine(expression);
+}
