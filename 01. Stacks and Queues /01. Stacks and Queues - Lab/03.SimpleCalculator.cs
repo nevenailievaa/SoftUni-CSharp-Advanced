a54834475dc855d19a593a2abd8df8b29e1481a6
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-11 14:44:59 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /01. Stacks and Queues - Lab/03.SimpleCalculator.cs
@@ -0,0 +1,25 @@
+﻿//INPUT
+string[] input = Console.ReadLine().Split().ToArray();
+
+//ACTION
+Stack<string> expression = new Stack<string>(input.Reverse());
+
+int result = int.Parse(expression.Pop());
+
+while (expression.Count >= 2)
+{
+    string sign = expression.Pop();
+    int number = int.Parse(expression.Pop());
+
+    if (sign == "+")
+    {
+        result += number;
+    }
+    else if (sign == "-")
+    {
+        result -= number;
+    }
+}
+
+//OUTPUT
+Console.WriteLine(result);
