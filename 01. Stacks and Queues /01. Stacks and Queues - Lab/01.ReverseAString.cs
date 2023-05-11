e83c789aa2d1605721206414f5f2677aebc9f3a8
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-11 13:44:24 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /01. Stacks and Queues - Lab/01.ReverseAString.cs
@@ -0,0 +1,13 @@
+﻿//INPUT
+string inputString = Console.ReadLine();
+
+//ACTION
+Stack<char> stack = new Stack<char>(inputString);
+
+//OUTPUT
+for (int i = stack.Count; i > 0; i--)
+{
+    char currChar = stack.Peek();
+    Console.Write(currChar);
+    stack.Pop();
+}
