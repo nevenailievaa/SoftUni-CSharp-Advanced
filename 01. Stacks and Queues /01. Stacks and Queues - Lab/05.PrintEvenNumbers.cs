79f83938a6f251a7e25e58964358e0763e9115a1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-13 13:04:17 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /01. Stacks and Queues - Lab/05.PrintEvenNumbers.cs
@@ -0,0 +1,19 @@
+﻿//iNPUT
+int[] integersInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//ACTION
+Queue<int> queue = new Queue<int>(integersInput);
+List<int> evenNums = new List<int>();
+
+for (int i = 0; i < integersInput.Length; i++)
+{
+    int currentNum = queue.Dequeue();
+
+	if (currentNum % 2 == 0)
+	{
+        evenNums.Add(currentNum);
+    }
+}
+
+//OUTOUT
+Console.WriteLine(String.Join(", ", evenNums));
