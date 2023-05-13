e93736c6c5c7ab4ede8569729b533c5b297e0ae6
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-14 00:07:08 +0300

    Add files via upload

---
+++ 01. Stacks and Queues/03.MaximumAndMinimumElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//INPUT
+int queriesCnt = int.Parse(Console.ReadLine());
+
+//ACTION
+Stack<int> numbers = new Stack<int>();
+
+for (int i = 1; i <= queriesCnt; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+	if (input[0] == 1)
+	{
+        numbers.Push(input[1]);
+	}
+	else if (input[0] == 2)
+	{
+        numbers.Pop();
+	}
+    else if (input[0] == 3)
+    {
+        if (numbers.Count > 0)
+        {
+            Console.WriteLine(numbers.Max());
+        }
+    }
+    else if (input[0] == 4)
+    {
+        if (numbers.Count > 0)
+        {
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
+
+//OUTPUT
+Console.WriteLine(String.Join(", ", numbers));
