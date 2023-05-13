6d830ad1c0c8ccceb10c1f2c21a6bd22c3601e47
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-13 13:04:53 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /01. Stacks and Queues - Lab/07.HotPotato.cs
@@ -0,0 +1,21 @@
+﻿//INPUT
+List<string> kidsArray = Console.ReadLine().Split().ToList();
+int repeats = int.Parse(Console.ReadLine());
+
+//ACTION
+Queue<string> kids = new Queue<string>(kidsArray);
+
+while (kids.Count != 1)
+{
+    for (int i = 1; i < repeats; i++)
+    {
+        string currentKid = kids.Dequeue();
+        kids.Enqueue(currentKid);
+    }
+
+    string removedKid = kids.Dequeue();
+    Console.WriteLine($"Removed {removedKid}");
+}
+
+//OUTPUT
+Console.WriteLine($"Last is {kids.Dequeue()}");
