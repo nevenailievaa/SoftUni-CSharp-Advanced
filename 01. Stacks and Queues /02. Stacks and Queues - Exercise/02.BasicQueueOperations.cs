c6ab918d9dc85d7e8c176112f12809896be9fab3
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-14 00:06:48 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /02. Stacks and Queues - Exercise/02.BasicQueueOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//INPUT
+int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int elementsToAddCnt = parameters[0];
+int elementsToRemoveCnt = parameters[1];
+int x = parameters[2];
+
+//ACTION
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Queue<int> queue = new Queue<int>(numbers);
+
+for (int i = 1; i <= elementsToRemoveCnt; i++)
+{
+	if (queue.Count > 0)
+	{
+        queue.Dequeue();
+    }
+}
+
+if (queue.Count == 0)
+{
+    Console.WriteLine(0);
+}
+else if (queue.Contains(x))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(queue.Min());
+}
