14e09623fa848aa2fe16603bb49053db25af6520
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-13 13:04:35 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /01. Stacks and Queues - Lab/06.Supermarket.cs
@@ -0,0 +1,21 @@
+﻿//INPUT
+string currentInput = string.Empty;
+
+//ACTION
+Queue<string> names = new Queue<string>();
+
+while ((currentInput = Console.ReadLine()) != "End")
+{
+	if (currentInput != "Paid")
+	{
+		names.Enqueue(currentInput);
+	}
+	else
+	{
+		Console.WriteLine(string.Join("\n", names));
+		names.Clear();
+	}
+}
+
+//OUTPUT
+Console.WriteLine($"{names.Count} people remaining.");
