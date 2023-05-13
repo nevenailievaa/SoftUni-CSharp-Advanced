8b4918ecbabcfa101fe421aa25940bedebd40592
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-13 13:05:06 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /01. Stacks and Queues - Lab/08.TrafficJam.cs
@@ -0,0 +1,36 @@
+﻿//INPUT
+int carsForGreenLight = int.Parse(Console.ReadLine());
+
+//ACTION
+Queue<string> cars = new Queue<string>();
+int passedCarsSum = 0;
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "end")
+{
+	if (command != "green")
+	{
+		cars.Enqueue(command);
+	}
+	else
+	{
+		if (cars.Count == 0)
+		{
+            continue;
+        }
+		for (int i = 1; i <= carsForGreenLight; i++)
+		{
+			string currentPassingCar = cars.Dequeue();
+			Console.WriteLine($"{currentPassingCar} passed!");
+			passedCarsSum++;
+
+			if (cars.Count == 0)
+			{
+				break;
+			}
+		}
+	}
+}
+
+//OUTPUT
+Console.WriteLine($"{passedCarsSum} cars passed the crossroads.");
