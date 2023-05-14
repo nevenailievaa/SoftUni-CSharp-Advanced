9a93c9dd6f7717e393e8a18d87e2dc9e19042568
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-15 01:20:25 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /02. Stacks and Queues - Exercise/04.FastFood.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//INPUT
+int foodQuantity = int.Parse(Console.ReadLine());
+int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//ACTION
+Queue<int> ordersQueue = new Queue<int>(orders);
+Console.WriteLine(ordersQueue.Max());
+
+for (int i = 0; i < orders.Length; i++)
+{
+	if (foodQuantity - ordersQueue.Peek() >= 0)
+	{
+        foodQuantity -= ordersQueue.Dequeue();
+    }
+	else
+	{
+		break;
+	}
+}
+
+//OUTPUT
+if (ordersQueue.Count == 0)
+{
+	Console.WriteLine("Orders complete");	
+}
+else
+{
+	Console.WriteLine($"Orders left: {String.Join(" ", ordersQueue)}");
+}
