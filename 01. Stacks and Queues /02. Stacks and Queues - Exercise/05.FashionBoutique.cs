5caf767457d5b796b8991bac307fd3a86c44e5ad
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-15 01:20:43 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /02. Stacks and Queues - Exercise/05.FashionBoutique.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//INPUT
+int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int rackCapacity = int.Parse(Console.ReadLine());
+
+Stack<int> clothesStack = new Stack<int>(clothes);
+int clothesSum = 0;
+int racksCnt = 1;
+
+while (clothesStack.Count > 0)
+{
+	if (clothesSum + clothesStack.Peek() < rackCapacity)
+	{
+		clothesSum += clothesStack.Pop();
+    }
+	else if (clothesSum + clothesStack.Peek() == rackCapacity)
+	{
+		clothesSum += clothesStack.Pop();
+
+		if (clothesStack.Count > 0)
+		{
+			clothesSum = 0;
+			racksCnt++;
+        }
+    }
+	else if (clothesSum + clothesStack.Peek() > rackCapacity)
+    {
+		clothesSum = 0;
+		racksCnt++;
+    }
+}
+
+//OUTPUT
+Console.WriteLine(racksCnt);
