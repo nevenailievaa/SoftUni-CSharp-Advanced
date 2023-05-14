fbfbbeb08fd9f7a6818a84059959a51ebbbf5f82
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-15 01:21:31 +0300

    Add files via upload

---
+++ 01. Stacks and Queues /02. Stacks and Queues - Exercise/08.BalancedParenthesis.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//INPUT
+char[] paranthesis = Console.ReadLine().ToCharArray(); //{[()]}
+Stack<char> paranthesisStack = new Stack<char>();
+
+//ACTION
+bool isValid = false;
+
+for (int i = 0; i < paranthesis.Length; i++)
+{
+    if (paranthesisStack.Count == 0)
+    {
+        if (paranthesis[i] == '}' || paranthesis[i] == ']' || paranthesis[i] == ')')
+        {
+            isValid = false;
+            break;
+        }
+    }
+
+	if (paranthesis[i] == '{' || paranthesis[i] == '[' || paranthesis[i] == '(')
+	{
+        paranthesisStack.Push(paranthesis[i]);
+    }
+    else if (paranthesis[i] == '}')
+    {
+        if (paranthesisStack.Peek() == '{')
+        {
+            paranthesisStack.Pop();
+            isValid = true;
+        }
+        else
+        {
+            isValid = false;
+            break;
+        }
+    }
+    else if (paranthesis[i] == ']')
+    {
+        if (paranthesisStack.Peek() == '[')
+        {
+            paranthesisStack.Pop();
+            isValid = true;
+        }
+        else
+        {
+            isValid = false;
+            break;
+        }
+    }
+    else if (paranthesis[i] == ')')
+    {
+        if (paranthesisStack.Peek() == '(')
+        {
+            paranthesisStack.Pop();
+            isValid = true;
+        }
+        else
+        {
+            isValid = false;
+            break;
+        }
+    }
+}
+
+//OUTPUT
+if (isValid)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
