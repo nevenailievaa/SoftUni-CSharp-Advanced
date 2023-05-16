fcfc50ced81a593f148e5bec2a71697ca0a38057
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-16 22:11:46 +0300

    Add files via upload

---
+++ 02. Multidimensional Arrays /01. Multidimensional Arrays - Lab/06.Jagged-ArrayModification.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+//INPUT
+int matrixRows = int.Parse(Console.ReadLine());
+
+//ACTION
+int[][] matrix = new int[matrixRows][];
+
+for (int i = 0; i < matrixRows; i++)
+{
+    matrix[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+}
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "END")
+{
+	string[] commandArray = command.Split().ToArray();
+
+	string commandType = commandArray[0];
+    int row = int.Parse(commandArray[1]);
+    int col = int.Parse(commandArray[2]);
+    int value = int.Parse(commandArray[3]);
+
+    if (matrix.Length > row && row >= 0 && matrix[row].Length > col && col >= 0)
+    {
+        if (commandType == "Add")
+        {
+            matrix[row][col] += value;
+        }
+        else if (commandType == "Subtract")
+        {
+            matrix[row][col] -= value;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid coordinates");
+    }
+}
+
+//OUTPUT
+foreach (int[] array in matrix)
+{
+    Console.WriteLine(string.Join(" ", array));
+}
