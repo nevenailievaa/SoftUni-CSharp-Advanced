48e67155fc736474c0902626919433afa2c6f02e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-16 19:19:06 +0300

    Add files via upload

---
+++ 02. Multidimensional Arrays /01. Multidimensional Arrays - Lab/03.PrimaryDiagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+//INPUT
+int matrixElementsCount = int.Parse(Console.ReadLine());
+int[,] matrix = new int[matrixElementsCount, matrixElementsCount];
+
+//ACTION
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] matrixElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = matrixElements[col];
+    }
+}
+
+int sum = 0;
+for (int i = 0; i < matrixElementsCount; i++)
+{
+    sum += matrix[i, i];
+}
+
+//OUTPUT
+Console.WriteLine(sum);
