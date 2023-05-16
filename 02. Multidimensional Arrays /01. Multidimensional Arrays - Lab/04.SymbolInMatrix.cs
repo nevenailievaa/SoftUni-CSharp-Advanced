ae935fc8b39a64495dc05c9eb92ebc35df2422f9
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-16 20:47:42 +0300

    Add files via upload

---
+++ 02. Multidimensional Arrays /01. Multidimensional Arrays - Lab/04.SymbolInMatrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+//INPUT
+int matrixElementsCount = int.Parse(Console.ReadLine());
+char[,] matrix = new char[matrixElementsCount, matrixElementsCount];
+
+//ACTION
+for (int row = 0; row < matrixElementsCount; row++)
+{
+    char[] matrixCharElements = Console.ReadLine().ToCharArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = matrixCharElements[col];
+    }
+}
+
+char symbol = char.Parse(Console.ReadLine());
+
+//Searching The Char
+for (int row = 0; row < matrixElementsCount; row++)
+{
+    for (int col = 0; col < matrixElementsCount; col++)
+    {
+        if (matrix[row, col] == symbol)
+        {
+            Console.WriteLine($"({row}, {col})");
+            return;
+        }
+    }
+}
+
+//OUTPUT
+Console.WriteLine($"{symbol} does not occur in the matrix");
