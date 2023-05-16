90b55f0e7b3601f6f0f821be60daf01e6325627a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-16 19:12:07 +0300

    Add files via upload

---
+++ 02. Multidimensional Arrays /01. Multidimensional Arrays - Lab/01.SumMatrixElements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+//INPUT
+int[] matrixElementsCount = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+int rows = matrixElementsCount[0];
+int cols = matrixElementsCount[1];
+int[,] matrix = new int[rows, cols];
+
+//ACTION
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] matrixElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+	{
+        matrix[row, col] = matrixElements[col];
+    }
+}
+
+int sumElements = 0;
+foreach (var item in matrix)
+{
+    sumElements += item;
+}
+
+//OUTPUT
+Console.WriteLine(matrix.GetLength(0));
+Console.WriteLine(matrix.GetLength(1));
+Console.WriteLine(sumElements);
