49e17cdd2e57179a71d93801ab4b1691929a1fd8
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-19 00:49:13 +0300

    Add files via upload

---
+++ 02. Multidimensional Arrays /02. Multidimensional Arrays - Exercise/05.SnakeMoves.cs
@@ -0,0 +1,42 @@
+﻿//INPUT
+int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+string snakeName = Console.ReadLine();
+int rows = dimensions[0];
+int cols = dimensions[1];
+
+//ACTION
+char[,] matrix = new char[rows, cols];
+
+//Creating The Snake's Matrix Path
+int index = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        if (row % 2 == 0)
+        {
+            matrix[row, col] = snakeName[index];
+        }
+        else
+        {
+            matrix[row, cols - 1 - col] = snakeName[index];
+        }
+
+        index++;
+
+        if (index >= snakeName.Length)
+        {
+            index = 0;
+        }
+    }
+}
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        Console.Write(matrix[row, col]);
+    }
+    Console.WriteLine();
+}
