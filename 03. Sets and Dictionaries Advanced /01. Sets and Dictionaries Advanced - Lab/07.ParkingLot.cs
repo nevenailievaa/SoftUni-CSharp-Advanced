faeee2292ef52a9564fe4c370fc84ba82735c92f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-20 21:28:01 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /01. Sets and Dictionaries Advanced - Lab/07.ParkingLot.cs
@@ -0,0 +1,37 @@
+﻿//INPUT
+string command = string.Empty;
+
+//ACTION
+HashSet<string> parking = new HashSet<string>();
+
+while((command = Console.ReadLine()) != "END")
+{
+    string[] carInfo = command.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+    string carDirection = carInfo[0];
+    string carNumber = carInfo[1];
+
+    if (carDirection == "IN")
+    {
+        parking.Add(carNumber);
+    }
+    else if (carDirection == "OUT")
+    {
+        if (parking.Contains(carNumber))
+        {
+            parking.Remove(carNumber);
+        }
+    }
+}
+
+//OUTPUT
+if (parking.Count > 0)
+{
+    foreach (var name in parking)
+    {
+        Console.WriteLine(name);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
