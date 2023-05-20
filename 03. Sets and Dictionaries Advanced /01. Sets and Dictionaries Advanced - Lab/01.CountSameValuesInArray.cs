a8ebedef67b75cb356eba2d69bb2b1b69b9c7626
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-20 14:10:59 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /01. Sets and Dictionaries Advanced - Lab/01.CountSameValuesInArray.cs
@@ -0,0 +1,28 @@
+﻿//INPUT
+double[] numbersInput = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToArray();
+
+//ACTION
+Dictionary<double, int> numbersOutput = new Dictionary<double, int>();
+
+for (int i = 0; i < numbersInput.Length; i++)
+{
+    double currentNum = numbersInput[i];
+
+    if (numbersOutput.ContainsKey(currentNum))
+    {
+        numbersOutput[currentNum]++;
+    }
+    else
+    {
+        numbersOutput.Add(currentNum, 1);
+    }
+}
+
+//OUTPUT
+foreach (var currentNum in numbersOutput)
+{
+    Console.WriteLine($"{currentNum.Key} - {currentNum.Value} times");
+}
