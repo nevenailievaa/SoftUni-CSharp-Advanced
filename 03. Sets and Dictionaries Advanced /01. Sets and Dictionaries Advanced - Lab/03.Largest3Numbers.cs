56061a42bae9cdf90c6eb17177943469a8081941
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-20 16:30:55 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /01. Sets and Dictionaries Advanced - Lab/03.Largest3Numbers.cs
@@ -0,0 +1,38 @@
+﻿//INPUT
+using System.Collections.Immutable;
+
+int[] integers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .OrderByDescending(n => n)
+    .ToArray();
+
+//OUTPUT
+if (integers.Length >= 3)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (i < 2)
+        {
+            Console.Write(integers[i] + " ");
+        }
+        else
+        {
+            Console.Write(integers[i]);
+        }
+    }
+}
+else
+{
+    for (int i = 0; i < integers.Length; i++)
+    {
+        if (i < integers.Length-1)
+        {
+            Console.Write(integers[i] + " ");
+        }
+        else
+        {
+            Console.Write(integers[i]);
+        }
+    }
+}
