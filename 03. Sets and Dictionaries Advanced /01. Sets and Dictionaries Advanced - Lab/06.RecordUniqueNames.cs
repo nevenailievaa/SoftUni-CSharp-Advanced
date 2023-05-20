8277676308d69de91540bc7ab9c3ba82395dcc9e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-20 21:27:44 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /01. Sets and Dictionaries Advanced - Lab/06.RecordUniqueNames.cs
@@ -0,0 +1,17 @@
+﻿//INPUT
+int namesCount = int.Parse(Console.ReadLine());
+
+//ACTION
+HashSet<string> names = new HashSet<string>();
+
+for (int i = 0; i < namesCount; i++)
+{
+    string name = Console.ReadLine();
+    names.Add(name);
+}
+
+//OUTPUT
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
