738a32179e7dcf9b57b967c21dddf4cec7a0ffe1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-21 12:35:58 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /02. Sets and Dictionaries Advanced - Exercise/01.UniqueUsernames.cs
@@ -0,0 +1,16 @@
+﻿//INPUT
+int userNamesCount = int.Parse(Console.ReadLine());
+
+//ACTION
+HashSet<string> userNames = new HashSet<string>();
+
+for (int i = 0; i < userNamesCount; i++)
+{
+    userNames.Add(Console.ReadLine());
+}
+
+//OUTPUT
+foreach (var user in userNames)
+{
+    Console.WriteLine(user);
+}
