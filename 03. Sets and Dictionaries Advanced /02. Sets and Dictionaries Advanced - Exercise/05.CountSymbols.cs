d1b699cb819395b0fc03ff72120485649d24dc49
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-21 13:12:14 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /02. Sets and Dictionaries Advanced - Exercise/05.CountSymbols.cs
@@ -0,0 +1,22 @@
+﻿//INPUT
+string text = Console.ReadLine();
+
+//ACTION
+SortedDictionary<char, int> charsAndRepeats = new SortedDictionary<char, int>();
+
+for (int i = 0; i < text.Length; i++)
+{
+	if (charsAndRepeats.ContainsKey(text[i]))
+	{
+		charsAndRepeats[text[i]]++;
+	}
+	else
+	{
+        charsAndRepeats.Add(text[i], 1);
+    }
+}
+
+foreach (var (character, repeats) in charsAndRepeats)
+{
+	Console.WriteLine($"{character}: {repeats} time/s");
+}
