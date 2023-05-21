7b45f0ca1778b7a7bb91aeefaba5c7c6df918b04
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-21 12:36:40 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /02. Sets and Dictionaries Advanced - Exercise/02.SetsOfElements.cs
@@ -0,0 +1,49 @@
+﻿//INPUT
+int[] lengths = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int setOneLength = lengths[0];
+int setTwoLength = lengths[1];
+
+//ACTION
+HashSet<int> setOne = new HashSet<int>();
+HashSet<int> setTwo = new HashSet<int>();
+HashSet<int> repeatElements = new HashSet<int>();
+
+//First Set
+for (int i = 0; i < setOneLength; i++)
+{
+    setOne.Add(int.Parse(Console.ReadLine()));
+}
+
+//Second Set
+for (int j = 0; j < setTwoLength; j++)
+{
+    setTwo.Add(int.Parse(Console.ReadLine()));
+}
+
+//Repeated Elements Finder
+if (setOneLength >= setTwoLength)
+{
+    foreach (var num in setOne)
+    {
+        if (setTwo.Contains(num))
+        {
+            repeatElements.Add(num);
+        }
+    }
+}
+else
+{
+    for (int i = 0; i < setTwoLength; i++)
+    {
+        foreach (var num in setTwo)
+        {
+            if (setOne.Contains(num))
+            {
+                repeatElements.Add(num);
+            }
+        }
+    }
+}
+
+//OUTPUT
+Console.WriteLine(String.Join(" ", repeatElements));
