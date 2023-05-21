abc16d1eb8951155ebd5eb7112bf903d1885f3a7
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-21 13:12:04 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /02. Sets and Dictionaries Advanced - Exercise/04.EvenTimes.cs
@@ -0,0 +1,29 @@
+﻿//INPUT
+int numbersCount = int.Parse(Console.ReadLine());
+
+//ACTION
+Dictionary<int,int> numbers = new Dictionary<int, int>();
+
+for (int i = 0; i < numbersCount; i++)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+
+	if (numbers.ContainsKey(currentNum))
+	{
+		numbers[currentNum]++;
+	}
+	else
+	{
+		numbers.Add(currentNum, 1);
+	}
+}
+
+//OUTPUT
+foreach (var number in numbers)
+{
+	if (number.Value % 2 == 0)
+	{
+		Console.WriteLine(number.Key);
+		return;
+	}
+}
