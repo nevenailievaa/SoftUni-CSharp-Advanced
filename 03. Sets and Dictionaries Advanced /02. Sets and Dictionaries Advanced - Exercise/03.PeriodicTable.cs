891649e1a62ab8db932d06ffe7fe605c4035329b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-21 12:36:53 +0300

    Add files via upload

---
+++ 03. Sets and Dictionaries Advanced /02. Sets and Dictionaries Advanced - Exercise/03.PeriodicTable.cs
@@ -0,0 +1,18 @@
+﻿//INPUT
+int elementsCount = int.Parse(Console.ReadLine());
+
+//ACTION
+HashSet<string> elements = new HashSet<string>();
+
+for (int i = 0; i < elementsCount; i++)
+{
+    string[] currentElements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    for (int j = 0; j < currentElements.Length; j++)
+    {
+        elements.Add(currentElements[j]);
+    }
+}
+
+//OUTPUT
+Console.WriteLine(String.Join(" ", elements.OrderBy(x => x)));
