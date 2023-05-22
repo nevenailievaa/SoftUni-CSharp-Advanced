d1f07d1e638060dabe5ebf93e29f349a5702513f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-22 14:00:43 +0300

    Update 07.KnightGame.cs

--- 02. Multidimensional Arrays /02. Multidimensional Arrays - Exercise/07.KnightGame.cs
+++ 02. Multidimensional Arrays /02. Multidimensional Arrays - Exercise/07.KnightGame.cs
@@ -2,13 +2,8 @@
 int size = int.Parse(Console.ReadLine());
 
 //ACTION
-if (size < 3)
-{
-    Console.WriteLine(0);
-    return;
-}
-
 char[,] chessBoard = new char[size, size];
+
 //Creating The Chess Board (The Matrix)
 for (int row = 0; row < size; row++)
 {
@@ -20,121 +15,137 @@ for (int row = 0; row < size; row++)
     }
 }
 
-int knightsRemoved = 0;
+int currentKills = 0;
+int bestKills = 0;
+int minimumKills = 0;
+int bestRow = 0;
+int bestCol = 0;
 
-while(true)
+while (true)
 {
-    int countMostAttacking = 0;
-    int rowMostAttacking = 0;
-    int colMostAttacking = 0;
-
     for (int row = 0; row < size; row++)
     {
         for (int col = 0; col < size; col++)
         {
-            if (chessBoard[row,col] == 'K')
-            {
-                int attackedKnights = CountAttackedKnights(row, col, size, chessBoard);
+            currentKills = CurrentKnightKills(chessBoard, row, col, size);
 
-                if (countMostAttacking < attackedKnights)
-                {
-                    countMostAttacking = attackedKnights;
-                    rowMostAttacking = row;
-                    colMostAttacking = col;
-                }
+            if (currentKills > bestKills)
+            {
+                bestKills = currentKills;
+                bestRow = row;
+                bestCol = col;
             }
+            currentKills = 0;
         }
     }
-    if (countMostAttacking == 0)
+
+    if (bestKills > 0)
     {
-        break;
+        chessBoard[bestRow, bestCol] = '0';
+        minimumKills++;
+        bestKills = 0;
     }
     else
     {
-        chessBoard[rowMostAttacking, colMostAttacking] = '0';
-        knightsRemoved++;
+        //OUTPUT
+        Console.WriteLine(minimumKills);
+        return;
     }
 }
 
-//OUTPUT
-Console.WriteLine(knightsRemoved);
 
-//Attacked Knights Counter
-static int CountAttackedKnights(int row, int col, int size, char[,] matrix)
+//Current Knight Kill Counter
+static int CurrentKnightKills(char[,]chessBoard, int row, int col, int size)
 {
-    int attackedKnights = 0;
+    int kills = 0;
+    //If Current Char Is Knight (K)
+    if (chessBoard[row, col] == 'K')
+    {
+        //Upper Left Move
+        if (isValid(row-2, col-1, size))
+        {
+            if (chessBoard[row-2, col-1] == 'K')
+            {
+                kills++;
+            }
+        }
 
-    //Horizontal Left - Up
-    if (IsCellValid(row-1, col-2, size))
-    {
-        if (matrix[row-1, col-2] == 'K')
+        //Upper Right Move
+        if (isValid(row-2, col+1, size))
         {
-            attackedKnights++;
+            if (chessBoard[row-2, col+1] == 'K')
+            {
+                kills++;
+            }
         }
-    }
-    //Horizontal Left - Down
-    if (IsCellValid(row+1, col-2, size))
-    {
-        if (matrix[row+1, col-2] == 'K')
-        {
-            attackedKnights++;
-        }
-    }
-    //Horizontal Right - Up
-    if (IsCellValid(row-1, col+2, size))
-    {
-        if (matrix[row-1, col+2] == 'K')
-        {
-            attackedKnights++;
-        }
-    }
-    //Horizontal Right - Down
-    if (IsCellValid(row+1, col+2, size))
-    {
-        if (matrix[row+1, col+2] == 'K')
-        {
-            attackedKnights++;
-        }
-    }
 
-    //Up-Left
-    if (IsCellValid(row-2, col-1, size))
-    {
-        if (matrix[row-2, col-1] == 'K')
+        //Horizontal Right Up Move
+        if (isValid(row-1, col+2, size))
         {
-            attackedKnights++;
+            if (chessBoard[row-1, col+2] == 'K')
+            {
+                kills++;
+            }
         }
-    }
-    //Up-Right
-    if (IsCellValid(row-2, col+1, size))
-    {
-        if (matrix[row-2, col+1] == 'K')
-        {
-            attackedKnights++;
-        }
-    }
-    //Down-Left
-    if (IsCellValid(row+2, col-1, size))
-    {
-        if (matrix[row+2, col-1] == 'K')
-        {
-            attackedKnights++;
-        }
-    }
-    //Down-Right
-    if (IsCellValid(row+2, col+1, size))
-    {
-        if (matrix[row+2, col+1] == 'K')
-        {
-            attackedKnights++;
-        }
-    }
 
-    return attackedKnights;
+        //Horizontal Right Down Move
+        if (isValid(row+1, col+2, size))
+        {
+            if (chessBoard[row+1, col+2] == 'K')
+            {
+                kills++;
+            }
+        }
+
+        //Horizontal Left Up Move
+        if (isValid(row-1, col-2, size))
+        {
+            if (chessBoard[row-1, col-2] == 'K')
+            {
+                kills++;
+            }
+        }
+
+        //Horizontal Left Down Move
+        if (isValid(row+1, col-2, size))
+        {
+            if (chessBoard[row+1, col-2] == 'K')
+            {
+                kills++;
+            }
+        }
+
+        //Down Left Move
+        if (isValid(row+2, col-1, size))
+        {
+            if (chessBoard[row+2, col-1] == 'K')
+            {
+                kills++;
+            }
+        }
+
+        //Down Right Move
+        if (isValid(row+2, col+1, size))
+        {
+            if (chessBoard[row+2, col+1] == 'K')
+            {
+                kills++;
+            }
+        }
+    }
+    return kills;
 }
 
-//Valid Cell Checker
-static bool IsCellValid(int row, int col, int size)
+
+//Valid Coordinates Checker
+static bool isValid(int row, int col, int size)
 {
-    return row >= 0 && row < size && col >= 0 && col < size;
+    if (row >= 0 && row < size && col >= 0 && col < size)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
