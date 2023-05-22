38bb59b6d319d6e21a5a6b9c1f6e3de340aeaa6a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-22 17:23:51 +0300

    Update 05.SnakeMoves.cs

--- 02. Multidimensional Arrays /02. Multidimensional Arrays - Exercise/05.SnakeMoves.cs
+++ 02. Multidimensional Arrays /02. Multidimensional Arrays - Exercise/05.SnakeMoves.cs
@@ -1,42 +1,43 @@
-﻿//INPUT
+//INPUT
 int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 string snakeName = Console.ReadLine();
 int rows = dimensions[0];
 int cols = dimensions[1];
 
 //ACTION
+Queue<char> snakeQueue = new Queue<char>(snakeName);
+
 char[,] matrix = new char[rows, cols];
 
 //Creating The Snake's Matrix Path
-int index = 0;
-
 for (int row = 0; row < rows; row++)
 {
-    for (int col = 0; col < cols; col++)
+    if (row % 2 == 0)
     {
-        if (row % 2 == 0)
+        for (int col = 0; col < cols; col++)
         {
-            matrix[row, col] = snakeName[index];
+            char currentChar = snakeQueue.Dequeue();
+            matrix[row, col] = currentChar;
+            snakeQueue.Enqueue(currentChar);
         }
-        else
+    }
+    else
+    {
+        for (int col = cols-1; col >= 0; col--)
         {
-            matrix[row, cols - 1 - col] = snakeName[index];
-        }
-
-        index++;
-
-        if (index >= snakeName.Length)
-        {
-            index = 0;
+            char currentChar = snakeQueue.Dequeue();
+            matrix[row, col] = currentChar;
+            snakeQueue.Enqueue(currentChar);
         }
     }
 }
 
+//Matrix Output
 for (int row = 0; row < rows; row++)
 {
     for (int col = 0; col < cols; col++)
     {
-        Console.Write(matrix[row, col]);
+        Console.Write(matrix[row,col]);
     }
     Console.WriteLine();
 }
