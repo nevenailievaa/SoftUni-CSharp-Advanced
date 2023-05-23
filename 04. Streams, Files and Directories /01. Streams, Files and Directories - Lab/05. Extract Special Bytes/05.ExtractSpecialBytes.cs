e6752432f35fe10044299fd4d45b0290df620969
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-23 23:23:15 +0300

    Add files via upload

---
+++ 04. Streams, Files and Directories /01. Streams, Files and Directories - Lab/05. Extract Special Bytes/05.ExtractSpecialBytes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            byte[] bytesToExtract = File.ReadAllText(bytesFilePath)
+                .Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(b => byte.Parse(b))
+                .ToArray();
+
+            using (FileStream inputStream = new FileStream(binaryFilePath, FileMode.Open))
+            using (FileStream outputStream = new FileStream(outputPath, FileMode.Create))
+            {
+                int currentByte;
+                while ((currentByte = inputStream.ReadByte()) != -1)
+                {
+                    if (bytesToExtract.Contains((byte)currentByte))
+                    {
+                        outputStream.WriteByte((byte)currentByte);
+                    }
+                }
+            }
+        }
+    }
+}
