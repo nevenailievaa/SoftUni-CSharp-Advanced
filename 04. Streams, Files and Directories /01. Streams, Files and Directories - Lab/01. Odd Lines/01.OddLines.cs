336b5f7862fa4b34fd1c63b292b207171af9e7fa
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-23 22:11:14 +0300

    Add files via upload

---
+++ 04. Streams, Files and Directories /01. Streams, Files and Directories - Lab/01. Odd Lines/01.OddLines.cs
@@ -0,0 +1,39 @@
+﻿namespace OddLines
+{
+    using System.IO;
+	
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int row = 0;
+
+                //Reading
+                while (!reader.EndOfStream)
+                {
+                    string currentRow = reader.ReadLine();
+
+                    //Writing
+                    using (StreamWriter writer = new StreamWriter(outputFilePath, true))
+                    {
+                        if (row % 2 == 1)
+                        {
+                            writer.WriteLine(currentRow);
+                        }
+                    }
+                    row++;
+                }
+            }
+        }
+    }
+}
