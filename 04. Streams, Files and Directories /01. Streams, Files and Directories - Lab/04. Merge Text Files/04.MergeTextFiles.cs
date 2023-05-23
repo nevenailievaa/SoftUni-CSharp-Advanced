85cc36154918bd8109e94b805369a95da10e3835
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-23 23:22:25 +0300

    Add files via upload

---
+++ 04. Streams, Files and Directories /01. Streams, Files and Directories - Lab/04. Merge Text Files/04.MergeTextFiles.cs
@@ -0,0 +1,47 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            using (var firstTextReader = new StreamReader(firstInputFilePath))
+            {
+                using (var secondTextReader = new StreamReader(secondInputFilePath))
+                {
+                    using (var writer = new StreamWriter(outputFilePath))
+                    {
+                        string line1 = string.Empty;
+                        string line2 = string.Empty;
+
+                        while ((line1 = firstTextReader.ReadLine()) != null && (line2 = secondTextReader.ReadLine()) != null)
+                        {
+                            if (line2 != null)
+                            {
+                                writer.WriteLine(line1);
+                            }
+
+                            if (line1 != null)
+                            {
+                                writer.WriteLine(line2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
