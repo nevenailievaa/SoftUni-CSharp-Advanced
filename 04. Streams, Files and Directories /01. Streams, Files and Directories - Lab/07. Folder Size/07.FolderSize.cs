da845bba2eb4318cd3b5cc3ade3717ae882138d2
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-23 23:25:07 +0300

    Add files via upload

---
+++ 04. Streams, Files and Directories /01. Streams, Files and Directories - Lab/07. Folder Size/07.FolderSize.cs
@@ -0,0 +1,28 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            using (var output = new StreamWriter(outputFilePath))
+            {
+                var files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+                decimal totalSize = files.Sum(file => new FileInfo(file).Length);
+
+                output.WriteLine($"{totalSize / 1024} KB");
+            }
+        }
+    }
+}
