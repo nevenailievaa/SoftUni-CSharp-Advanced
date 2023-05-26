da916d07b2a75ac6a5164214720d635a4ef95907
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-26 17:34:46 +0300

    Add files via upload

---
+++ 05. Functional Programming /01. Functional Programming - Lab/04.AddVAT.cs
@@ -0,0 +1,16 @@
+﻿//INPUT
+Func<double, double> addingVat = n => n + (n * 0.2);
+
+double[] prices = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToArray();
+
+//ACTION
+for (int i = 0; i < prices.Length; i++)
+{
+    prices[i] = addingVat(prices[i]);
+
+    //OUTPUT
+    Console.WriteLine($"{prices[i]:f2}");
+}
