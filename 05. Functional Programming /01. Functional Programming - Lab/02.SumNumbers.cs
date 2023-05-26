151afcb202d40f7f83dbd26c0a46311d6f2a4a82
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-26 16:12:21 +0300

    Add files via upload

---
+++ 05. Functional Programming /01. Functional Programming - Lab/02.SumNumbers.cs
@@ -0,0 +1,6 @@
+﻿Func<string, int> parser = n => int.Parse(n);
+
+int[] numbers = Console.ReadLine().Split(", ").Select(parser).ToArray();
+
+Console.WriteLine(numbers.Length);
+Console.WriteLine(numbers.Sum());
