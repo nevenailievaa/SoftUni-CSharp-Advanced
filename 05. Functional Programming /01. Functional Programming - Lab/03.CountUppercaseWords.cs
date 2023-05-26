52ac34b9266334a0b244c14d2e63d3b8c6c59d33
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-26 17:34:34 +0300

    Add files via upload

---
+++ 05. Functional Programming /01. Functional Programming - Lab/03.CountUppercaseWords.cs
@@ -0,0 +1,10 @@
+﻿//INPUT
+Func<string, bool> isUpperFunc = w => Char.IsUpper(w[0]);
+
+string[] words = Console.ReadLine()
+	.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+	.Where(isUpperFunc)
+	.ToArray();
+
+//OUTPUT
+Console.WriteLine(String.Join("\n", words));
