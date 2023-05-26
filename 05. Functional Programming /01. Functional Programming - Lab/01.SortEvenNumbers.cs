962c2686a83110f54ce0a3a6f68dbc8367a2a9ad
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-26 16:12:01 +0300

    Add files via upload

---
+++ 05. Functional Programming /01. Functional Programming - Lab/01.SortEvenNumbers.cs
@@ -0,0 +1,10 @@
+﻿//INPUT
+int[] integers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Where(n => n % 2 == 0)
+    .OrderBy(n => n)
+    .ToArray();
+
+//OUTPUT
+Console.WriteLine(String.Join(", ", integers));
