ad32044414ebc7e87cd8b31727f05dbe727ea5f1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-28 16:37:38 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/06.ReverseAndExclude.cs
@@ -0,0 +1,23 @@
+﻿//INPUT
+List<int> inputNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Reverse()
+    .ToList();
+
+int divider = int.Parse(Console.ReadLine());
+
+//ACTION
+Predicate<int> isDivisible = number => number % divider == 0;
+
+for (int i = 0; i < inputNumbers.Count; i++)
+{
+    if (isDivisible(inputNumbers[i]))
+    {
+        inputNumbers.RemoveAt(i);
+        i--;
+    }
+}
+
+//OUTPUT
+Console.WriteLine(String.Join(" ", inputNumbers));
