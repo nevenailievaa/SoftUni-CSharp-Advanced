ea757668b594e68f79559a3ef18bcc3adc71ac2f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-28 14:13:24 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/02.KnightsOfHonor.cs
@@ -0,0 +1,9 @@
+﻿//INPUT
+Func<string, string[]> readingNames = names => names.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(name => "Sir" + " " + name)
+.ToArray();
+string[] words = readingNames(Console.ReadLine());
+
+//OUTPUT
+Action<string[]> printer = names => Console.WriteLine(string.Join("\n", names));
+printer(words);
