70801f22fa01782f93039b357ee7b56c35dc095f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-28 16:37:50 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/07.PredicateForNames.cs
@@ -0,0 +1,19 @@
+﻿//INPUT
+int nameLength = int.Parse(Console.ReadLine());
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+//ACTION
+Predicate<string> nameChecker = name => name.Length <= nameLength;
+
+//OUTPUT
+List<string> validNames = new List<string>();
+for (int i = 0; i < names.Length; i++)
+{
+	if (nameChecker(names[i]))
+	{
+		validNames.Add(names[i]);
+	}
+}
+
+//OUTPUT
+Console.WriteLine(string.Join("\n", validNames));
