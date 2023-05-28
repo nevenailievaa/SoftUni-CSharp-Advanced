e732ce7d409ab2e322a65f112e33fc76bebdad8b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-28 14:13:34 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/03.CustomMinFunction.cs
@@ -0,0 +1,10 @@
+﻿//INPUT
+Func<string, int[]> readIntegers = input => input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(int.Parse)
+.ToArray();
+
+int[] numbers = readIntegers(Console.ReadLine());
+
+//OUTPUT
+Action<int[]> output = numbers => Console.WriteLine(numbers.Min());
+output(numbers);
