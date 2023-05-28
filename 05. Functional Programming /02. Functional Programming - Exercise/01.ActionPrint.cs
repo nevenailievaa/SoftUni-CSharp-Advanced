1e22f1191ff49142c99d3aec3ecd24af3a6951b2
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-28 14:13:13 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/01.ActionPrint.cs
@@ -0,0 +1,7 @@
+﻿//INPUT
+Func<string, string[]> readingStrings = words => words.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+string[] words = readingStrings(Console.ReadLine());
+
+//OUTPUT
+Action<string[]> printer = words => Console.WriteLine(string.Join("\n", words));
+printer(words);
