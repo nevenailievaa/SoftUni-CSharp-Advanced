78ab8c56b8343ffb7143a8339ddc7c6c66d35373
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-28 14:13:58 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/05.AppliedArithmetics.cs
@@ -0,0 +1,46 @@
+﻿//INPUT
+Func<string, int[]> readIntegers = input => input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(int.Parse)
+.ToArray();
+
+int[] numbers = readIntegers(Console.ReadLine());
+
+//Commands
+Func<int, int> calculator = null;
+Action<int> printer = null;
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "end")
+{
+    //Printing
+    if (command == "print")
+    {
+        Console.WriteLine(String.Join(" ", numbers));
+    }
+
+    //Calculations
+    else
+    {
+        calculator = number =>
+        {
+            if (command == "add")
+            {
+                number = number += 1;
+            }
+            else if (command == "multiply")
+            {
+                number = number *= 2;
+            }
+            else if (command == "subtract")
+            {
+                number = number -= 1;
+            }
+            return number;
+        };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = calculator(numbers[i]);
+        }
+    }
+}
