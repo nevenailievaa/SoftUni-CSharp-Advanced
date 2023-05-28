1681ba8dba2e193204bd6ae5635409cb9fc42a01
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-28 14:13:47 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/04.FindEvensOrOdds.cs
@@ -0,0 +1,36 @@
+﻿//INPUT
+Func<string, int[]> readIntegers = input => input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(int.Parse)
+.ToArray();
+
+int[] inputNumbers = readIntegers(Console.ReadLine());
+int lowerBond = inputNumbers[0];
+int upperBond = inputNumbers[1];
+
+//ACTION
+string command = Console.ReadLine();
+Predicate<int> numbersSelector = number =>
+{
+    if (command == "even")
+    {
+        return number % 2 == 0;
+    }
+    else if (command == "odd")
+    {
+        return number % 2 != 0;
+    }
+    return false;
+};
+
+List<int> outputNumbers = new List<int>();
+
+for (int i = lowerBond; i <= upperBond; i++)
+{
+    if (numbersSelector(i))
+    {
+        outputNumbers.Add(i);
+    }
+}
+
+//OUTPUT
+Console.WriteLine(String.Join(" ", outputNumbers));
