5873b67a927943b6f517a3353e729be9204f36a9
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-29 14:02:47 +0300

    Add files via upload

---
+++ 05. Functional Programming /02. Functional Programming - Exercise/11.TriFunction.cs
@@ -0,0 +1,22 @@
+﻿//INPUT
+int givenSum = int.Parse(Console.ReadLine());
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+foreach (string name in names)
+{
+    Func<string, int, bool> isValidName = (name, nameLength) =>
+    {
+        int nameSum = 0;
+        for (int i = 0; i < nameLength; i++)
+        {
+            nameSum += name[i];
+        }
+        return nameSum >= givenSum;
+    };
+
+    if (isValidName(name, name.Length))
+    {
+        Console.WriteLine(name);
+        return;
+    }
+}
