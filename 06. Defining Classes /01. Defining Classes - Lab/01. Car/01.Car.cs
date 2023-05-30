f9cf90d7c24f2e959167a5785c0ee4f7fac37a3f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-05-30 18:31:11 +0300

    Add files via upload

---
+++ 06. Defining Classes /01. Defining Classes - Lab/01. Car/01.Car.cs
@@ -0,0 +1,16 @@
+﻿using CarManufacturer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Car currentCar = new Car();
+
+        currentCar.Make = "VW";
+        currentCar.Model = "MK3";
+        currentCar.Year = 1992;
+
+        Console.WriteLine($"Make: {currentCar.Make}\nModel: {currentCar.Model}\nYear: {currentCar.Year}");
+    }
+}
+
