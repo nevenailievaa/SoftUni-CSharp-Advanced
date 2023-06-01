86b9960deed114a90c2dbe99420e76e4701ab176
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-01 18:36:13 +0300

    Add files via upload

---
+++ 06. Defining Classes /02. Defining Classes - Exercise/06. Speed Racing/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace DefiningClasses;
+
+internal class StartUp
+{
+    static void Main(string[] args)
+    {
+        //INPUT
+        int carsCount = int.Parse(Console.ReadLine());
+
+        //ACTION
+        Dictionary<string, Car> carsByNames = new Dictionary<string, Car>();
+
+        for (int i = 0; i < carsCount; i++)
+        {
+            string[] carInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string model = carInfo[0];
+            double fuelAmount = double.Parse(carInfo[1]);
+            double fuelConsumptionPerKilometer = double.Parse(carInfo[2]);
+
+            Car car = new Car(model, fuelAmount, fuelConsumptionPerKilometer, 0);
+            carsByNames.Add(model, car);
+        }
+
+        //Commands
+        string command = string.Empty;
+
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string[] carInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string model = carInfo[1];
+            double amountOfKm = double.Parse(carInfo[2]);
+
+            Car currentCar = carsByNames[model];
+            currentCar.Drive(amountOfKm);
+        }
+
+        //OUTPUT
+        foreach (var car in carsByNames.Values)
+        {
+            Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+        }
+    }
+}
