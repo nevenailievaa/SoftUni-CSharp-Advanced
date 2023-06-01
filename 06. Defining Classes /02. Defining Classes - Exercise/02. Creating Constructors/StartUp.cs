83a8baf585dd8604de119fcf6047d104040af991
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-01 15:20:41 +0300

    Add files via upload

---
+++ 06. Defining Classes /02. Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses;
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person defaultPerson = new Person();
+
+        Person personCustomAge = new Person();
+        personCustomAge.Age = 20;
+
+        Person customPerson = new Person();
+        customPerson.Name = "Nevena";
+        customPerson.Age = 19;
+
+        Console.WriteLine($"Default data person's name is {defaultPerson.Name} and is {defaultPerson.Age} years old.");
+
+        Console.WriteLine($"Custom age person's name is {personCustomAge.Name} and is {personCustomAge.Age} years old.");
+
+        Console.WriteLine($"Custom person's name is {customPerson.Name} and is {customPerson.Age} years old.");
+    }
+}
