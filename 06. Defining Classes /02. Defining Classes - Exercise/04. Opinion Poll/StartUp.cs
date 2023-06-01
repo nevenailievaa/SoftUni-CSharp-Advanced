fea744957a26ee5c49a37268f9ebdef7c2153e4c
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-01 17:29:44 +0300

    Add files via upload

---
+++ 06. Defining Classes /02. Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            int count = int.Parse(Console.ReadLine());
+
+            //ACTION
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                people.Add(new Person(name, age));
+            }
+
+            //OUTPUT
+            foreach (var person in people.OrderBy(p => p.Name).Where(p => p.Age > 30))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
