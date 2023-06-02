2efbb3ad098eb104a7a4a047c783a60e21e0d76a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-02 16:14:13 +0300

    Add files via upload

---
+++ 06. Defining Classes /02. Defining Classes - Exercise/08. Car Salesman/Car.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace DefiningClasses;
+
+public class Car
+{
+    public string Model { get; set; }
+    public Engine Engine { get; set; }
+    public int Weight { get; set; }
+    public string Color { get; set; }
+
+    //Constructors
+    public Car(string model, Engine engine)
+    {
+        Model = model;
+        Engine = engine;
+    }
+    public Car(string model, Engine engine, int weight, string color) :this(model, engine)
+    {
+        Weight = weight;
+        Color = color;
+    }
+
+    //ToString Method
+    public override string ToString()
+    {
+        string weight = Weight == 0 ? "n/a" : Weight.ToString();
+        string color = Color ?? "n/a";
+
+
+        StringBuilder sb = new();
+
+        sb.AppendLine($"{Model}:");
+        sb.AppendLine($"  {Engine.ToString()}");
+        sb.AppendLine($"  Weight: {weight}");
+        sb.AppendLine($"  Color: {color}");
+
+        return sb.ToString().TrimEnd();
+    }
+}
---
+++ 06. Defining Classes /02. Defining Classes - Exercise/08. Car Salesman/Engine.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace DefiningClasses;
+
+public class Engine
+{
+    public string Model { get; set; }
+    public int Power { get; set; }
+    public int Displacement { get; set; }
+    public string Efficiency { get; set; }
+
+    //Constructors
+    public Engine(string model, int power)
+    {
+        Model = model;
+        Power = power;
+    }
+    public Engine(string model, int power, int displacement, string efficiency):this(model, power)
+    {
+        Displacement = displacement;
+        Efficiency = efficiency;
+    }
+
+    //ToString Method
+    public override string ToString()
+    {
+        string displacement = Displacement == 0 ? "n/a" : Displacement.ToString();
+        string efficiency = Efficiency ?? "n/a";
+
+        StringBuilder sb = new();
+
+        sb.AppendLine($"{Model}:");
+        sb.AppendLine($"    Power: {Power}");
+        sb.AppendLine($"    Displacement: {displacement}");
+        sb.AppendLine($"    Efficiency: {efficiency}");
+
+        return sb.ToString().TrimEnd();
+    }
+}
