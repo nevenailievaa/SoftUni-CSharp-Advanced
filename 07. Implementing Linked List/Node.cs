ab9108777428486e06d0d1e7e0e29a20ac9a2812
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-04 18:09:47 +0300

    Add files via upload

---
+++ 07. Implementing Linked List/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomDoublyLinkedList;
+
+public class Node
+{
+    public int Value { get; set; }
+    public Node Next { get; set; }
+    public Node Previous { get; set; }
+
+    //Constructor
+    public Node(int value, Node next = null, Node previous = null)
+    {
+        Value = value;
+        Next = next;
+        Previous = previous;
+    }
+}
