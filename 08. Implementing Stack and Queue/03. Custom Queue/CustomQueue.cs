b22ce8f6e4588a67a7a1420cdb7097c712c2c409
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-05 12:21:07 +0300

    Add files via upload

---
+++ 08. Implementing Stack and Queue/03. Custom Queue/CustomQueue.cs
@@ -0,0 +1,125 @@
+﻿namespace CustomStructures;
+
+internal class CustomQueue
+{
+    public int[] Items { get; set; }
+    public int Count { get; private set; }
+
+    private const int Capacity = 4;
+
+
+    //Constructor
+    public CustomQueue()
+    {
+        Items = new int[Capacity];
+    }
+
+    //Public Methods
+    public void Enqueue(int value)
+    {
+        if (Count == Items.Length-1)
+        {
+            Resize();
+        }
+
+        Items[Count] = value;
+        Count++;
+    }
+
+    public int Dequeue()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("CustomQueue is empty!");
+        }
+        else if (Count <= Items.Length / 4)
+        {
+            Shrink();
+        }
+
+        int removedElement = Items[0];
+        ShiftLeft();
+        Count--;
+
+        return removedElement;
+    }
+
+    public int Peek()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("CustomQueue is empty!");
+        }
+
+        return Items[0];
+    }
+
+    public void Clear()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("CustomQueue is empty!");
+        }
+
+        for (int i = 0; i < Count; i++)
+        {
+            Items[i] = default;
+        }
+        Count = 0;
+    }
+
+    public void ForEach(Action<int> action)
+    {
+        if (Items.Length == 0)
+        {
+            throw new InvalidOperationException("CustomQueue is empty!");
+        }
+
+        for (int i = 0; i < Count; i++)
+        {
+            action(Items[i]);
+        }
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < Count-1; i++)
+        {
+            Console.Write(Items[i] + ", ");
+        }
+        Console.WriteLine(Items[Count-1]);
+    }
+
+    //Private Methods
+    private void Resize()
+    {
+        int[] copy = new int[Items.Length * 2];
+
+        for (int i = 0; i < Count; i++)
+        {
+            copy[i] = Items[i];
+        }
+
+        Items = copy;
+    }
+
+    private void Shrink()
+    {
+        int[] copy = new int[Items.Length / 2];
+
+        for (int i = 0; i < Count; i++)
+        {
+            copy[i] = Items[i];
+        }
+
+        Items = copy;
+    }
+
+    private void ShiftLeft()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            Items[i] = Items[i+1];
+        }
+    }
+}
---
+++ 08. Implementing Stack and Queue/03. Custom Queue/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using CustomStructures;
+using System.Xml.Linq;
+
+CustomQueue queue = new CustomQueue();
+string command = String.Empty;
+
+while ((command = Console.ReadLine()) != "End")
+{
+    string[] commandInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+    string commandType = commandInfo[0];
+
+    if (commandType == "Enqueue")
+    {
+        int value = int.Parse(commandInfo[1]);
+        queue.Enqueue(value);
+        Console.WriteLine($"Successfully enqueued {value} to the queue!");
+    }
+    else if (commandType == "Dequeue")
+    {
+        int element = queue.Dequeue();
+
+        if (queue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty.");
+            continue;
+        }
+
+        Console.WriteLine($"Successfully dequeued the first element of the queue! ({element})");
+    }
+    else if (commandType == "Peek")
+    {
+        int element = queue.Peek();
+        Console.WriteLine($"The first element of the queue is: {element}.");
+    }
+    else if (commandType == "Clear")
+    {
+        queue.Clear();
+        Console.WriteLine($"The queue has been succesfully cleared.");
+        continue;
+    }
+    else if (commandType == "ForEach")
+    {
+        queue.ForEach(element => Console.Write(element*2 + " "));
+        Console.WriteLine();
+        continue;
+    }
+
+    queue.Print();
+}
