01b70392868164487e377919163bde0b9f9b79fe
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-06 20:03:42 +0300

    Add files via upload

---
+++ 09. Generics/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericScale;
+
+public class EqualityScale<T> where T : IComparable<T>
+{
+    private T Left;
+    private T Right;
+
+    //Constructor
+    public EqualityScale(T left, T right)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    public bool AreEqual()
+    {
+        return Left.Equals(Right);
+    }
+}
---
+++ 09. Generics/03. Generic Scale/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            EqualityScale<int> ints = new EqualityScale<int>(a, b);
+
+            if (ints.AreEqual())
+            {
+                Console.WriteLine("Numbers are equal!");
+            }
+            else
+            {
+                Console.WriteLine("Numbers are not equal!");
+            }
+
+
+            string Astring = Console.ReadLine();
+            string Bstring = Console.ReadLine();
+
+            EqualityScale<string> strings = new EqualityScale<string>(Astring, Bstring);
+
+            if (strings.AreEqual())
+            {
+                Console.WriteLine("Strings are equal!");
+            }
+            else
+            {
+                Console.WriteLine("Strings are not equal!");
+            }
+        }
+    }
+}
