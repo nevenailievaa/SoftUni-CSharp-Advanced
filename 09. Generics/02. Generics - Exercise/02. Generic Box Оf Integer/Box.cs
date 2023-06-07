b14588ae7e52ce93dae522073420b1965e1abbc1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-07 14:53:23 +0300

    Add files via upload

---
+++ 09. Generics/02. Generics - Exercise/02. Generic Box Оf Integer/Box.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace GenericBoxОfString;
+
+public class Box<T>
+{
+    private List<T> Items { get; set; }
+
+    //Constructor
+    public Box()
+    {
+        Items = new List<T>();
+    }
+
+    //Methods
+    public void Add(T item)
+    {
+        Items.Add(item);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+
+        foreach (var item in Items)
+        {
+            sb.AppendLine($"{typeof(T)}: {item}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
---
+++ 09. Generics/02. Generics - Exercise/02. Generic Box Оf Integer/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GenericBoxОfString;
+
+//INPUT
+int count = int.Parse(Console.ReadLine());
+
+//ACTION
+Box<int> box = new Box<int>();
+
+for (int i = 0; i < count; i++)
+{
+    int item = int.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+
+Console.WriteLine(box.ToString());
