f8caf92a33126dd438d6ecdc42c1e1250d7fc035
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-07 14:53:39 +0300

    Add files via upload

---
+++ 09. Generics/02. Generics - Exercise/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GenericBoxОfString;
+
+//INPUT
+int count = int.Parse(Console.ReadLine());
+
+//ACTION
+Box<string> box = new Box<string>();
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+
+    box.Add(item);
+}
+
+int[] indexes = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+box.Swap(indexes[0], indexes[1]);
+Console.WriteLine(box.ToString());
