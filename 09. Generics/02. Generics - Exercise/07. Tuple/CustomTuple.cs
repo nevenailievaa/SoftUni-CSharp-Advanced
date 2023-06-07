3031398be5ba0da2235ac5aabeb91052f0e948cf
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-07 15:54:51 +0300

    Add files via upload

---
+++ 09. Generics/02. Generics - Exercise/07. Tuple/CustomTuple.cs
@@ -0,0 +1,20 @@
+﻿namespace Tuple;
+
+internal class CustomTuple<T1, T2>
+{
+    private T1 ItemOne { get; set; }
+    private T2 ItemTwo { get; set; }
+
+    //Constructor
+    public CustomTuple(T1 itemOne, T2 itemTwo)
+    {
+        ItemOne = itemOne;
+        ItemTwo = itemTwo;
+    }
+
+    //Method
+    public override string ToString()
+    {
+        return $"{ItemOne} -> {ItemTwo}";
+    }
+}
---
+++ 09. Generics/02. Generics - Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using Tuple;
+
+//INPUT
+string[] nameAndAdressInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+string[] nameAndBeerInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+string[] numbersInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+//ACTION
+CustomTuple<string, string> nameAndAdress =
+    new CustomTuple<string, string>($"{nameAndAdressInfo[0]} {nameAndAdressInfo[1]}", nameAndAdressInfo[2]);
+
+CustomTuple<string, int> nameAndBeer =
+    new CustomTuple<string, int>(nameAndBeerInfo[0], int.Parse(nameAndBeerInfo[1]));
+
+CustomTuple<int, double> numbers =
+    new CustomTuple<int, double>(int.Parse(numbersInfo[0]), double.Parse(numbersInfo[1]));
+
+//OUTPUT
+Console.WriteLine(nameAndAdress.ToString());
+Console.WriteLine(nameAndBeer.ToString());
+Console.WriteLine(numbers.ToString());
+
