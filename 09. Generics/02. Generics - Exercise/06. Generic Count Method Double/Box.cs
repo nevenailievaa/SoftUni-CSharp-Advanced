9a41950e06666d24f569c966997b986d2a0105a0
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-07 14:54:36 +0300

    Add files via upload

---
+++ 09. Generics/02. Generics - Exercise/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace GenericBoxОfString;
+
+public class Box<T> where T : IComparable<T>
+{
+    private List<T> Items { get; set; }
+
+    //Constructor
+    public Box()
+    {
+        Items = new List<T>();
+    }
+
+    //Methods
+    public void Add(T item)
+    {
+        Items.Add(item);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+
+        foreach (var item in Items)
+        {
+            sb.AppendLine($"{typeof(T)}: {item}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public void Swap(int indexOne, int indexTwo)
+    {
+        T currentItem = Items[indexOne];
+
+        Items[indexOne] = Items[indexTwo];
+        Items[indexTwo] = currentItem;
+    }
+
+    public int Compare(T itemToCompare)
+    {
+        int count = 0;
+
+        foreach (var item in Items)
+        {
+            if (item.CompareTo(itemToCompare) > 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ 09. Generics/02. Generics - Exercise/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GenericBoxОfString;
+
+//INPUT
+int count = int.Parse(Console.ReadLine());
+
+//ACTION
+Box<double> box = new Box<double>();
+
+for (int i = 0; i < count; i++)
+{
+    double item = double.Parse(Console.ReadLine());
+
+    box.Add(item);
+}
+
+double itemToCompare = double.Parse(Console.ReadLine());
+
+Console.WriteLine(box.Compare(itemToCompare));
