bf905cd7ef8cfe333b3f2dac5fdd1cad21c1b2ef
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-07 14:54:17 +0300

    Add files via upload

---
+++ 09. Generics/02. Generics - Exercise/05. Generic Count Method String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GenericBoxОfString;
+
+//INPUT
+int count = int.Parse(Console.ReadLine());
+
+//ACTION
+Box<string> box = new Box<string>();
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+
+    box.Add(item);
+}
+
+string itemToCompare = Console.ReadLine();
+
+Console.WriteLine(box.Compare(itemToCompare));
