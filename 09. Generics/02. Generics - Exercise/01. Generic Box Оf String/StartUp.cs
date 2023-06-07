47168a56dd0b9393e3db0c352209616448f2b612
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-07 14:53:06 +0300

    Add files via upload

---
+++ 09. Generics/02. Generics - Exercise/01. Generic Box Оf String/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GenericBoxОfString;
+
+//INPUT
+int count = int.Parse(Console.ReadLine());
+
+//ACTION
+Box<string> box = new Box<string>();
+
+for (int i = 0; i < count; i++)
+{
+    string item = Console.ReadLine();
+
+    box.Add(item);
+}
+
+Console.WriteLine(box.ToString());
