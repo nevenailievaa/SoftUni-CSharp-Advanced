4989b4c3707389a0d0456768c69d9029e82f1c8e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-11 10:15:32 +0300

    Add files via upload

---
+++ 10. Iterators and Comparators/02. Iterators and Comparators - Exercise/03. Stack/CustomStack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CustomStack;
+
+public class CustomStack<T> : IEnumerable<T>
+{
+    //Constructor
+    public CustomStack()
+    {
+        Items = new List<T>();
+        Counter = 0;
+    }
+
+    //Properties
+    private List<T> Items { get; set; }
+    public int Counter { get; private set; }
+
+    //Methods
+    public void Push(T value)
+    {
+        Items.Add(value);
+        Counter++;
+    }
+    public T Pop()
+    {
+        T value = Items[Items.Count-1];
+        Items.RemoveAt(Items.Count-1);
+        Counter--;
+        return value;
+    }
+
+    //IEnumerable Methods
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = Items.Count-1; i >= 0; i--)
+        {
+            yield return Items[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
---
+++ 10. Iterators and Comparators/02. Iterators and Comparators - Exercise/03. Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using CustomStack;
+using System.Timers;
+
+//INPUT
+string[] input = Console.ReadLine()
+    .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+    .Skip(1)
+    .ToArray();
+
+CustomStack<string> stack = new CustomStack<string>();
+
+foreach (var element in input)
+{
+    stack.Push(element);
+}
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "END")
+{
+    if (command == "Pop")
+    {
+        if (stack.Counter <= 0)
+        {
+            Console.WriteLine("No elements");
+            continue;
+        }
+        stack.Pop();
+    }
+    else if (command.Contains("Push"))
+    {
+        string[] commandArray = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+        stack.Push(commandArray[1]);
+    }
+}
+
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
