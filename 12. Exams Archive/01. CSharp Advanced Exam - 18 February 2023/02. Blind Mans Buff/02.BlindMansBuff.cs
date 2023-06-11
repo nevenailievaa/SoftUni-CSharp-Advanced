3a5d4fa3f115406e047a49eeaa24e8adca5b74ae
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-11 16:16:01 +0300

    Add files via upload

---
+++ 12. Exams Archive/01. CSharp Advanced Exam - 18 February 2023/02. Blind Mans Buff/02.BlindMansBuff.cs
@@ -0,0 +1,82 @@
+﻿//INPUT
+int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int rows = dimensions[0];
+int cols = dimensions[1];
+
+string[,] playGround = new string[rows,cols];
+int playerRow = 0;
+int playerCol = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    string[] playgroundInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        playGround[row, col] = playgroundInfo[col];
+
+        if (playgroundInfo[col] == "B")
+        {
+            playerRow = row;
+            playerCol = col;
+        }
+    }
+}
+
+//ACTION
+int touchedOpponents = 0;
+int movesMade = 0;
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Finish")
+{
+    //Validity Checker
+    if (command == "up")
+    {
+        if (playerRow - 1 >= 0 && playGround[playerRow-1, playerCol] != "O")
+        {
+            playerRow--;
+            movesMade++;
+        }
+    }
+    else if (command == "down")
+    {
+        if (playerRow + 1 < rows && playGround[playerRow+1, playerCol] != "O")
+        {
+            playerRow++;
+            movesMade++;
+        }
+    }
+    else if (command == "left")
+    {
+        if (playerCol - 1 >= 0 && playGround[playerRow, playerCol-1] != "O")
+        {
+            playerCol--;
+            movesMade++;
+        }
+    }
+    else if (command == "right")
+    {
+        if (playerCol + 1 < cols && playGround[playerRow, playerCol+1] != "O")
+        {
+            playerCol++;
+            movesMade++;
+        }
+    }
+
+    //Touched Player Checker
+    if (playGround[playerRow,playerCol] == "P")
+    {
+        touchedOpponents++;
+        playGround[playerRow, playerCol] = "-";
+    }
+
+    if (touchedOpponents == 3)
+    {
+        break;
+    }
+}
+
+//OUTPUT
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {touchedOpponents} Moves made: {movesMade}");
