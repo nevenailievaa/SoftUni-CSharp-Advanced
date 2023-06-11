a628355ba682176f0a81a5551135f0a350277f31
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-11 10:16:01 +0300

    Add files via upload

---
+++ 10. Iterators and Comparators/02. Iterators and Comparators - Exercise/05. Comparing Objects/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace ComparingObjects;
+
+internal class Person : IComparable<Person>
+{
+    private string Name { get; set; }
+    private int Age { get; set; }
+    private string Town { get; set; }
+
+    //Constructor
+    public Person(string name, int age, string town)
+    {
+        Name = name;
+        Age = age;
+        Town = town;
+    }
+
+    //IComparable Method
+    public int CompareTo(Person otherPerson)
+    {
+        int result = Name.CompareTo(otherPerson.Name);
+
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = Age.CompareTo(otherPerson.Age);
+
+        if (result != 0)
+        {
+            return result;
+        }
+
+        return Town.CompareTo(otherPerson.Town);
+    }
+}
---
+++ 10. Iterators and Comparators/02. Iterators and Comparators - Exercise/05. Comparing Objects/Program.cs
@@ -0,0 +1,34 @@
+﻿using ComparingObjects;
+using System;
+
+string command = string.Empty;
+List<Person> people = new List<Person>();
+
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] personInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    Person currentPerson = new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]);
+    people.Add(currentPerson);
+}
+
+int index = int.Parse(Console.ReadLine()) -1;
+Person personToCompare = people[index];
+
+int matchesCount = 0;
+
+foreach(Person person in people)
+{
+    if (person.CompareTo(personToCompare) == 0)
+    {
+        matchesCount++;
+    }
+}
+if (matchesCount == 1)
+{
+    Console.WriteLine("No matches");
+}
+else
+{
+    Console.WriteLine($"{matchesCount} {people.Count-matchesCount} {people.Count}");
+}
