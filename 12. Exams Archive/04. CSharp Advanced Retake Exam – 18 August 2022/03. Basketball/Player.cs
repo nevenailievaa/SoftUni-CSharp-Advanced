7f12965ab7f08803e7cf40f17262d767b356fe9e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-13 13:37:44 +0300

    Add files via upload

---
+++ 12. Exams Archive/04. CSharp Advanced Retake Exam – 18 August 2022/03. Basketball/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Basketball
+{
+    public class Player
+    {
+        //Constructor
+        public Player(string name, string position, double rating, int games)
+        {
+            Name = name;
+            Position = position;
+            Rating = rating;
+            Games = games;
+        }
+
+        //Properties
+        public string Name { get; private set; }
+        public string Position { get; private set; }
+        public double Rating { get; private set; }
+        public int Games { get; private set; }
+        public bool Retired { get; set; } = false;
+
+        //Methods
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"-Player: {Name}");
+            sb.AppendLine($"--Position: {Position}");
+            sb.AppendLine($"--Rating: {Rating}");
+            sb.AppendLine($"--Games played: {Games}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
