6d9c096d50c3abf322d1785d39bd813c6a165199
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-15 12:49:45 +0300

    Add files via upload

---
+++ 12. Exams Archive/06. CSharp Advanced Retake Exam - 13 April 2022/01. Meal Plan/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.MealPlan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            Queue<string> meals = new Queue<string>(Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .ToArray());
+
+            Stack<int> calories = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            Dictionary<string, int> mealsTable = new Dictionary<string, int>()
+            {
+                {"salad", 350 },
+                {"soup", 490 },
+                {"pasta", 680 },
+                {"steak", 790 }
+            };
+
+            //ACTION
+            int mealsAte = 0;
+
+            while (meals.Any() && calories.Any())
+            {
+                string currentMeal = meals.Peek();
+                int currentCalories = calories.Peek();
+
+                bool isAte = false;
+
+                //Meals
+                if (mealsTable.ContainsKey(currentMeal))
+                {
+                    isAte = true;
+                }
+
+                //Is Ate Check
+                if (isAte)
+                {
+                    mealsAte++;
+                    currentCalories -= mealsTable[currentMeal];
+                    meals.Dequeue();
+
+                    if (currentCalories == 0)
+                    {
+                        calories.Pop();
+                    }
+                    else if (currentCalories < 0)
+                    {
+                        int additionalCalories = Math.Abs(currentCalories);
+                        calories.Pop();
+
+                        if (calories.Any())
+                        {
+                            int nextDayCalories = calories.Pop() - additionalCalories;
+                            calories.Push(nextDayCalories);
+                        }
+                    }
+                    else
+                    {
+                        calories.Pop();
+                        calories.Push(currentCalories);
+                    }
+                }
+            }
+
+            //OUTPUT
+            if (meals.Any())
+            {
+                Console.WriteLine($"John ate enough, he had {mealsAte} meals.");
+                Console.WriteLine($"Meals left: {String.Join(", ", meals)}.");
+            }
+            else
+            {
+                Console.WriteLine($"John had {mealsAte} meals.");
+                Console.WriteLine($"For the next few days, he can eat {String.Join(", ", calories)} calories.");
+            }
+        }
+    }
+}
