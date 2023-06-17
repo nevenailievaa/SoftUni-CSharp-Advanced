f3af8fd760e48466a465b704f8b9aa00edb17e17
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-17 19:09:20 +0300

    Add files via upload

---
+++ 13. CSharp Advanced Regular Exam - 17 June 2023/03. Automotive Repair Shop/RepairShop.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        //Properties
+        public int Capacity { get; private set; }
+        public List<Vehicle> Vehicles { get; set; }
+
+        //Constructor
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            Vehicles = new List<Vehicle>();
+        }
+
+        //Methods
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (Vehicles.Count < Capacity)
+            {
+                Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin)
+        {
+            Vehicle vehicle = Vehicles.Find(v => v.VIN == vin);
+            if (vehicle != null)
+            {
+                Vehicles.Remove(vehicle);
+                return true;
+            }
+            return false;
+        }
+        
+        public int GetCount()
+        {
+            return Vehicles.Count;
+        }
+
+        public Vehicle GetLowestMileage() => Vehicles.Find(x => x.Mileage == Vehicles.Min(y => y.Mileage));
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Vehicles in the preparatory:");
+
+            foreach (Vehicle vehicle in Vehicles)
+            {
+                sb.AppendLine(vehicle.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 13. CSharp Advanced Regular Exam - 17 June 2023/03. Automotive Repair Shop/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿namespace AutomotiveRepairShop
+{
+    public class Vehicle
+    {
+        //Constructor
+        public Vehicle(string vin, int mileage, string damage)
+        {
+            VIN = vin;
+            Mileage = mileage;
+            Damage = damage;
+        }
+
+        //Properties
+        public string VIN { get; private set; }
+        public int Mileage { get; private set; }
+        public string Damage { get; private set; }
+
+        //Methods
+        public override string ToString()
+        {
+            return $"Damage: {Damage}, Vehicle: {VIN} ({Mileage} km)";
+        }
+    }
+}
