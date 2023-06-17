d27c6f72f68ad79617aa897e13885c44711af236
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-06-17 19:07:40 +0300

    Add files via upload

---
+++ 13. CSharp Advanced Regular Exam - 17 June 2023/01. Temple Of Doom/Program.cs
@@ -0,0 +1,66 @@
+﻿//INPUT
+Queue<int> tools = new Queue<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray());
+
+Stack<int> substances = new Stack<int>(Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray());
+
+List<int> challengesArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+//ACTION
+while (tools.Any() && substances.Any() && challengesArray.Count > 0)
+{
+    int sum = tools.Peek() * substances.Peek();
+
+    if (challengesArray.Contains(sum))
+    {
+        tools.Dequeue();
+        substances.Pop();
+        challengesArray.Remove(sum);
+    }
+    else
+    {
+        int toolToMove = tools.Dequeue() + 1;
+        tools.Enqueue(toolToMove);
+
+        int substanceToDecrease = substances.Pop() - 1;
+        if (substanceToDecrease > 0)
+        {
+            substances.Push(substanceToDecrease);
+        }
+    }
+}
+
+//OUTPUT
+if (challengesArray.Count > 0)
+{
+    Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+}
+else
+{
+    Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+}
+
+//Print
+if (tools.Any())
+{
+    Console.Write("Tools: ");
+    Console.WriteLine(String.Join(", ", tools));
+}
+if (substances.Any())
+{
+    Console.Write("Substances: ");
+    Console.WriteLine(String.Join(", ", substances));
+}
+if (challengesArray.Count > 0)
+{
+    Console.Write("Challenges: ");
+    Console.WriteLine(String.Join(", ", challengesArray));
+}
